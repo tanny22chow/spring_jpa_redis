f52ba51962d52712a71637b89b584022636ae96c
e090392 <tanmoy22chowdhury@gmail.com>
2019-12-20 12:54:08 -0600

    adding pom

---
+++ springredisdemo/src/main/java/com/springPrac/springredisdemo/SpringredisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springPrac.springredisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringredisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringredisdemoApplication.class, args);
+	}
+
+}
